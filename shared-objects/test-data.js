44b52bbf81151cec7320c082317f940f877ef92a
savenok-ivan <savenok.ivan@gmail.com>
2018-04-26 14:49:55 +0300

    Update test-data.js

--- shared-objects/test-data.js
+++ shared-objects/test-data.js
@@ -1,9 +1,10 @@
 module.exports = {
     username: "Dzmitry",
     password: "l}0S'Yt>]!1})E%4?=-{$Ft\\$.~@*3ZJ",
-    nameButton: 'New Button',
+    nameButton: 'New Button' + Date.now(),
     keywordAutoLabeling: 'test keyword contains',
     labelAutoLabeling: 'testing contains',
     operatorAutoLabeling: 'CONTAIN',
+    commentFeedbackText: "Test feedback" + Date.now(),
     emailForFeedback: "alexey.bazhin.jazzteam@gmail.com"
 };
