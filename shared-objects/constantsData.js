b069073681731744b2bbb738f70adca20ecfa4fb
savenok-ivan <savenok.ivan@gmail.com>
2018-05-03 11:55:28 +0300

    Create constantsData.js

---
+++ shared-objects/constantsData.js
@@ -0,0 +1,7 @@
+module.exports = {
+    oneSecondTimeOut: 1000,
+    twoSecondsTimeOut: 2 * 1000,
+    threeSecondsTimeOut: 3 * 1000,
+    fourSecondsTimeOut: 4 * 1000,
+    twoMinutesTimeout: 120 * 1000
+};
