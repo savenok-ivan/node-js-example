5489bb811b1372d9914f4927867168b29992d81c
savenok-ivan <savenok.ivan@gmail.com>
2018-05-03 11:56:25 +0300

    Update usabillaSetup.js

--- page-objects/usabillaSetup.js
+++ page-objects/usabillaSetup.js
@@ -1,43 +1,39 @@
+/* eslint-disable no-undef */
 module.exports = {
-    
     elements: {
         linkToCreateNewButton: by.css('.add_new.tooltips'),
-        nameButtonInput: by.name('site_form[name]'),
-        submitNewButton: by.css('#button_setup > button.link_button.gradient.rounded_button.green_button'),
-        advancedSettingsLink: by.className('advanced'),
-        generateFullPageScreenshotCheckbox: by.id('site_form_full_page_screenshot'),
-        inputKeywordAutoLabeling: by.id('site_form_auto_label_0_auto_label_text'),
-        inputLabelAutoLabeling: by.id('site_form_auto_label_0_auto_label_name'),
-        selectOperatorAutoLabeling: by.id('site_form_auto_label_0_auto_label_operator')
+        nameButtonInput: by.css('#site_form_name'),
+        submitNewButton: by.css('#button_setup .green_button'),
+        advancedSettingsLink: by.css('.advanced'),
+        generateFullPageScreenshotCheckbox: by.css('#site_form_full_page_screenshot'),
+        inputKeywordAutoLabeling: by.css('#site_form_auto_label_0_auto_label_text'),
+        inputLabelAutoLabeling: by.css('#site_form_auto_label_0_auto_label_name'),
+        selectOperatorAutoLabeling: by.css('#site_form_auto_label_0_auto_label_operator')
     },
     navigateToButtonCreationPage: function () {
         driver.findElement(page.usabillaSetup.elements.linkToCreateNewButton).click();
     },
 
     createNewButton: function () {
-        var nameButton = shared["test-data"].nameButton;
+        var nameButton = shared.testData.nameButton;
         driver.findElement(page.usabillaSetup.elements.nameButtonInput).sendKeys(nameButton);
     },
 
     generateFullPageScreenshot: function () {
-        driver.sleep(1000);
+        driver.sleep(shared.constantsData.fourSecondsTimeOut);
         driver.findElement(page.usabillaSetup.elements.advancedSettingsLink).click();
         driver.findElement(page.usabillaSetup.elements.generateFullPageScreenshotCheckbox).click();
     },
 
-    setUpAutoLabeling: function () {
-        var keywordAutoLabeling = shared["test-data"].keywordAutoLabeling;
-        var labelAutoLabeling = shared["test-data"].labelAutoLabeling;
-        var operatorAutoLabeling = shared["test-data"].operatorAutoLabeling;
-        
-        driver.sleep(1000);
-        driver.findElement(page.usabillaSetup.elements.inputKeywordAutoLabeling).sendKeys(keywordAutoLabeling);
-        driver.findElement(page.usabillaSetup.elements.inputLabelAutoLabeling).sendKeys(labelAutoLabeling);
-        driver.findElement(page.usabillaSetup.elements.selectOperatorAutoLabeling).sendKeys(operatorAutoLabeling);
+    setUpAutoLabeling: function (keyword, label, operator) {
+        driver.sleep(shared.constantsData.oneSecondTimeOut);
+        driver.findElement(page.usabillaSetup.elements.inputKeywordAutoLabeling).sendKeys(keyword);
+        driver.findElement(page.usabillaSetup.elements.inputLabelAutoLabeling).sendKeys(label);
+        driver.findElement(page.usabillaSetup.elements.selectOperatorAutoLabeling).sendKeys(operator);
     },
 
     saveButton: function () {
-        driver.sleep(1000);
+        driver.sleep(shared.constantsData.oneSecondTimeOut);
         driver.findElement(page.usabillaSetup.elements.submitNewButton).click();
     }
 };
