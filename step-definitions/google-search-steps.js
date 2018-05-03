8f6c6ea12d658579df6d29063787be00da79833b
savenok-ivan <savenok.ivan@gmail.com>
2018-05-03 11:54:36 +0300

    Update google-search-steps.js

--- step-definitions/google-search-steps.js
+++ step-definitions/google-search-steps.js
@@ -1,138 +1,105 @@
+/* eslint-disable no-trailing-spaces */
 module.exports = function () {
-    
-    this.setDefaultTimeout(60*1000);
+    this.setDefaultTimeout(60 * 1000);
     
     this.Before(function () {
-        console.log(" ----------- Before")
-    })
+        return console.log(' ----------- Before ----------- ');
+    });
 
     this.After(function () {
-        console.log(" ----------- After")
-    })
+        console.log(' ----------- After ----------- ');
+        driver.quit();
+    });
 
-
-    this.When("Login to the Usabilla environment", function () {
-        return helpers.loadPage("https://app-staging.usabilla.com").then(function() {
-            page.usabillaLogin.signIn();
+    this.Given(/^Login to the Usabilla \("([^"]*)"\) \(login: "([^"]*)", password: "([^"]*)"\)\.$/, function (url, login, password) {
+        helpers.loadPage(url).then(function() {
+            console.log(' ----------- Done - Open site ----------- ');
+            page.usabillaLogin.signIn(login, password);
+            console.log(' ----------- Done - Login to the Usabilla ----------- ');
         });
     });
 
-    this.Then("Navigate to the 'Usabilla for Websites' button creation page", function () {
-        return helpers.loadPage("https://app-staging.usabilla.com/member/live/setup").then(function() {
+    //------------------------------------------------------------------------------------------------------------------
+    /* Scenario: Creation */
+    //------------------------------------------------------------------------------------------------------------------
+    this.When(/^Navigate to the button creation page \("([^"]*)"\)\.$/, function (url) {
+        helpers.loadPage(url).then(function() {
             page.usabillaSetup.navigateToButtonCreationPage();
+            console.log(' ----------- Done - Navigate to the button creation page ----------- ');
+
         });
     });
 
-    this.Then("Feedback button: Create a new button and assign a name", function () {
+    this.When('Create a new button and assign a name.', function () {
         page.usabillaSetup.createNewButton();
+        console.log(' ----------- Done - Create a new button and assign a name ----------- ');
+
     });
 
-    this.Then("Button advanced setting: Turn on full-page screenshot generation for the newly created button", function () {
+    this.Then('Turn on full-page screenshot generation for the newly created button.', function () {
         page.usabillaSetup.generateFullPageScreenshot();
+        console.log(' ----------- Done - Turn on full-page screenshot generation for the newly created button. ----------- ');
+
     });
 
-    this.Then("Button advanced setting: Set up Auto labeling for keyword 'test keyword contains' with the label 'testing contains'. Set operator to Contain.", function () {
-        page.usabillaSetup.setUpAutoLabeling();
+    this.Then(/^Set up Auto labeling for keyword "([^"]*)" with the label "([^"]*)"\. Set operator to "([^"]*)"\.$/, function (keyword, label, operator) {
+        page.usabillaSetup.setUpAutoLabeling(keyword, label, operator);
+        console.log(' ----------- Done - Set up Auto labeling for keyword ----------- ');
+
     });
 
-    this.Then("Feedback button: Save the button", function () {
+    this.Then('Save the button.', function () {
         page.usabillaSetup.saveButton();
+        console.log(' ----------- Done Save the button. - ----------- ');
+
     });
 
-    this.Then("Feedback button: Provide feedback through the feedback button and its form which were created in the steps 3 through 6.", function () {
-        page.usabillaFeedback.feedbackThroughFeedbackButton();
+    this.Then(/^Provide feedback through the feedback button and its form which were created \(email: "([^"]*)"\)\.$/, function (email) {
+        page.usabillaFeedback.feedbackThroughFeedbackButton(email);
+        console.log(' ----------- Done -  Provide feedback through the feedback button and its form which were created----------- ');
+
     });
 
-    this.Then("Feedback page: Load the page which lists all the feedback items", function () {
-        helpers.loadPage("https://app-staging.usabilla.com/member/#/websites/buttons/feedback/");
+    //------------------------------------------------------------------------------------------------------------------
+    /* Scenario: Analysis */
+    //------------------------------------------------------------------------------------------------------------------
+    this.When(/^Load the page which lists all the feedback items \("([^"]*)"\)\.$/, function (url) {
+        return helpers.loadPage(url);
     });
 
-    this.Then(/^Feedback page: Confirm that the newly submitted feedback item is present \(last comment text "([^"]*)"\)$/, function (expectedFeedbackName) {
-        page.usabillaFeedback.checkSubmittedFeedbackIitemIsPresent(expectedFeedbackName);
+    this.Then('Confirm that the newly submitted feedback item is present.', function () {
+        return page.usabillaFeedback.checkSubmittedFeedbackIitemIsPresent();
     });
 
-    this.Then("Feedback item: Open the detailed view of the feedback item", function () {
-        page.usabillaFeedback.openDetailedViewOfFeedbackItem();
+    this.When('Open the detailed view of the feedback item.', function () {
+        return page.usabillaFeedback.openDetailedViewOfFeedbackItem();
     });
 
-
-    //--------------------------------------------------------------------------------------------------------------------------------------
-
-
-    this.Then("Feedback item: Check if the generated screenshot is present with the feedback item", function () {
-        driver.sleep(1000);
-        driver.findElement(by.css('.screenshot .screenshot__image')).getCssValue('width').then(function (width) {
-            driver.findElement(by.css('.screenshot .screenshot__image')).getCssValue('height').then(function (height) {
-                return assert.isTrue(+(width.slice(0, -2)) > 0 && +(height.slice(0, -2)) > 0, "Generated screenshot is not present with the feedback item");
-            });
-        });
+    this.Then('Check if the generated screenshot is present with the feedback item.', function () {
+        return page.usabillaFeedback.checkGeneratedScreenshotOfFeedbackItem();
     });
 
-    this.Then("Feedback item: Manually add two labels to an individual feedback item. Refresh page to see that label is present.", function () {
-        driver.sleep(1000);
-        page.usabillaFeedback.addTwoLabelToFeedbackItem();
-        driver.navigate().refresh();
-        driver.sleep(2000);
-        return driver.findElement(by.id('select-tag-dropdown-84')).getText().then(function (text) {
-            return assert.equal(text, "2 labels", "Labels is not present.");
-        });
+    this.When('Manually add two labels to feedback item. Refresh page to see that label is present.', function () {
+        return page.usabillaFeedback.checkAddTwoLabelToFeedbackItem();
     });
 
-    this.Then("Feedback item: Remove one label from the same feedback item. Refresh page to see that label is removed.", function () {
-        driver.sleep(1000);
-        page.usabillaFeedback.removeOneLabelOfFeedbackItem();
-        driver.navigate().refresh();
-        driver.sleep(2000);
-        return driver.findElement(by.id('select-tag-dropdown-84')).getText().then(function (text) {
-            return assert.equal(text, "1 label", "Label is not remove.");
-        });
+    this.When('Remove one label from the same feedback item. Refresh page to see that label is removed.', function () {
+        return page.usabillaFeedback.checkRemoveOneLabelOfFeedbackItem();
     });
 
-    this.Then("Feedback page: Enter a custom date through the calendar", function () {
-        return helpers.loadPage("https://app-staging.usabilla.com/member/#/websites/buttons/feedback/").then(function() {
-            //return driver.wait(until.elementsLocated(by.partialLinkText(keywords)), 10000);
-            // use a method on the page object which also returns a promise
-            //return page.googleSearch.enterDateThroughCalendar();
-        });
+    this.Then(/^Enter a custom date through the calendar \("([^"]*)"\)\.$/, function (url) {
+        return helpers.loadPage(url);
     });
 
-    this.Then("Feedback page: Filter on the score provided (optional validate score inside individual feedback item)", function () {
-        /*return helpers.loadPage("https://app-staging.usabilla.com/member/#/websites/buttons/feedback/").then(function() {
-         //return driver.wait(until.elementsLocated(by.partialLinkText(keywords)), 10000);
-         // use a method on the page object which also returns a promise
-         return page.googleSearch.enterDateThroughCalendar();
-         });*/
+    this.Then('Filter on the score provided.', function () {});
+
+    this.Then('Filter on the Net Promoter Score.', function () {});
+
+    this.Then('Filter on specific user comment left in test case 7.', function () {
+        return page.usabillaFeedback.checkCommentCreatedFeedback();
     });
 
-    this.Then("Feedback page: Filter on the Net Promoter Score", function () {
-        /*return helpers.loadPage("https://app-staging.usabilla.com/member/#/websites/buttons/feedback/").then(function() {
-         //return driver.wait(until.elementsLocated(by.partialLinkText(keywords)), 10000);
-         // use a method on the page object which also returns a promise
-         return page.googleSearch.enterDateThroughCalendar();
-         });*/
+    this.Then('Filter on a label added in test case 12.', function () {
+        return page.usabillaFeedback.checkLabelCreatedFeedback();
     });
-
-    this.Then("Feedback page: Filter on specific user comment left in test case 7.", function () {
-        var commentFeedbackText = shared["test-data"].commentFeedbackText;
-
-        page.usabillaFeedback.filterOnUserComment();
-
-        return driver.findElement(by.css("list-container > div > list-item:nth-child(1) > div a.feedback-item__comment-text")).getText().then(function (text) {
-            return assert.equal(text, commentFeedbackText, "Feedback with such comment not found");
-        });
-    });
-
-    this.Then("Feedback page: Filter on a label added in test case 12", function () {
-        console.log("-----------------------------");
-        console.log(page.usabillaFeedback.labelFeedback);
-        console.log("-----------------------------");
-        /*var commentFeedbackText = shared["test-data"].commentFeedbackText;
-
-         page.googleSearch.filterOnUserComment();
-
-         return driver.findElement(by.css("list-container > div > list-item:nth-child(1) > div a.feedback-item__comment-text")).getText().then(function (text) {
-         return assert.equal(text, commentFeedbackText, "Feedback with such comment not found");
-         });*/
-    });
-    
 };
